665bb86ff274a3f35f85e27fb68b6afdb4e93bd9
yusufovamina <yusufovamina@gmail.com>
2025-09-08 14:37:46 +0400

    port forwarding

--- web/next.config.ts
+++ web/next.config.ts
@@ -5,6 +5,18 @@ const nextConfig: NextConfig = {
     // Do not block production builds on ESLint errors in pages we didn't edit now
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5155/api/:path*",
+      },
+      {
+        source: "/chat-api/:path*",
+        destination: "http://localhost:5000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
